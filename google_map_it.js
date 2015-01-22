fb9cc7ab2b1d3648a70327fcaf834ab3c8ff072a
Josh Hannan <joshh@aycmedia.com>
2015-01-22 15:21:13 -0500

    Updated Code

--- google_map_it.js
+++ google_map_it.js
@@ -1,65 +1,56 @@
 (function($) {
 	$.fn.google_map_it = function( options ) {
-
-		var object = $(this);
-
 		var settings = $.extend({
-			address: "'1330 Pine Street, Philadelphia, PA 19107'",
-			coordinates: '39.945259, -75.164128',
-			zoom: 10,
-			directions: 'no'
+			locations: {
+				location: {
+					lat: '41.6706',
+					lng: '-71.2783',
+					title: 'Title'
+				}
+			}
 		}, options );
+		var marker_title;
+		var latlng;
+		var image = '/images/map_marker.png';
+		var markers;
+		var marker;
+		var i;
+		var locations_count = settings.locations.length;
+		var styles = [{'featureType':'landscape','stylers':[{'saturation':-100},{'lightness':65},{'visibility':'on'}]},{'featureType':'poi','stylers':[{'saturation':-100},{'lightness':51},{'visibility':'simplified'}]},{'featureType':'road.highway','stylers':[{'saturation':-100},{'visibility':'simplified'}]},{'featureType':'road.arterial','stylers':[{'saturation':-100},{'lightness':30},{'visibility':'on'}]},{'featureType':'road.local','stylers':[{'saturation':-100},{'lightness':40},{'visibility':'on'}]},{'featureType':'transit','stylers':[{'saturation':-100},{'visibility':'simplified'}]},{'featureType':'administrative.province','stylers':[{'visibility':'off'}]},{'featureType':'water','elementType':'labels','stylers':[{'visibility':'on'},{'lightness':-25},{'saturation':-100}]},{'featureType':'water','elementType':'geometry','stylers':[{'hue':'#ffff00'},{'lightness':-25},{'saturation':-97}]}];
 
-		object.each(function() {
-			/*
-			var directionsDisplay;
-			var directionsService = new google.maps.DirectionsService();
-			*/
+		$(this).each(function() {
 			var map;
-
-			function initialize() {
-				directionsDisplay = new google.maps.DirectionsRenderer();
-				var geoLocation = "'"+settings.coordinates+"'";
-				console.log(geoLocation);
-				var coordinates = new google.maps.LatLng(geoLocation);
-				var mapOptions = {
-					zoom: 10,
-					disableDefaultUI: true,
-					mapTypeControlOptions: {
-						style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-					},
-					center: coordinates,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				};
-				map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-				var marker = new google.maps.Marker({
-					position: coordinates,
-					map: map,
-					title: 'Testing Company'
+			var bounds = new google.maps.LatLngBounds();
+			var mapOptions = {
+				zoom: 6,
+				styles: styles,
+				scrollwheel: false,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			map = new google.maps.Map( document.getElementById('google-map'), mapOptions );
+			var infowindow = new google.maps.InfoWindow();
+			for( i = 0; i < locations_count; i++ ) {
+				marker = new google.maps.Marker({
+					position: new google.maps.LatLng( settings.locations[i]['curlat'], settings.locations[i]['curlng'] ),
+					icon: image,
+					map: map
 				});
-				directionsDisplay.setPanel(document.getElementById('g-directions'));
-			}
-
-			/*
-			function calcRoute() {
-				var start = document.getElementById('start_address').value;
-				var end = end_address;
-				var request = {
-					origin: start,
-					destination: end,
-					travelMode: google.maps.DirectionsTravelMode.DRIVING
-				};
-				directionsService.route(request, function(response, status) {
-					if (status == google.maps.DirectionsStatus.OK) {
-						directionsDisplay.setDirections(response);
+				console.log(image);
+				bounds.extend(marker.position);
+				google.maps.event.addListener(marker, 'click', (function(marker, i) {
+					var info = '<h3>'+settings.locations[i]['curtitle']+'</h3><p>'+settings.locations[i]['curaddress'];
+					if( settings.locations[i]['curaddress2'].length > 0 ) {
+						info += '</br>'+settings.locations[i]['curaddress2'];
 					}
-				});
+					info += '<br/>'+settings.locations[i]['curcity']+', '+settings.locations[i]['curstate']+' '+settings.locations[i]['curzip']+'</p>';
+					return function() {
+						infowindow.setContent( info );
+						infowindow.open(map, marker);
+					}
+				})(marker, i));
 			}
-			*/
-			google.maps.event.addDomListener(window, 'load', initialize);
+			map.fitBounds(bounds);
 		});
-
 		return this;
-
-	}; //  END GOOGLE_MAP_IT FUNCTION
+	}; //  END CHECKOUT_ACCORDION FUNCTION
 }(jQuery));
